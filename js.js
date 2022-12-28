2aed2b2d87d22acd60fff1143e5b9012e570c722
Agustina-Gutierrez <61707564+Agustina-Gutierrez@users.noreply.github.com>
2022-12-28 20:38:39 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,30 @@
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "¡Gracias por tu mensaje, espero conocerte pronto!";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Ocurrio un error"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Ocurrio un error"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
